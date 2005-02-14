5930e3e181432aa89f75458bc6cd0cefc57a5214
mrjohnson <mrjohnson@86e5652c-ad1d-0410-bc08-f5e3af449c86>
2005-02-14 21:33:55 +0000

    set license for .java files to libpng. lgpl has some onerous problems. i don't really care about these files, and this license fits that.
    i wanted to do just "public domain" but i guess that legal standing is
    somewhat shaky.

--- jep/Run.java
+++ jep/Run.java
@@ -7,19 +7,28 @@ import java.io.*;
  * <pre>
  * Run.java - Execute a Python script.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Copyright (c) 2004, 2005 Mike Johnson.
+ *
+ * This file is licenced under the the zlib/libpng License.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any
+ * damages arising from the use of this software.
  * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * Permission is granted to anyone to use this software for any
+ * purpose, including commercial applications, and to alter it and
+ * redistribute it freely, subject to the following restrictions:
  * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *     1. The origin of this software must not be misrepresented; you
+ *     must not claim that you wrote the original software. If you use
+ *     this software in a product, an acknowledgment in the product
+ *     documentation would be appreciated but is not required.
+ * 
+ *     2. Altered source versions must be plainly marked as such, and
+ *     must not be misrepresented as being the original software.
+ * 
+ *     3. This notice may not be removed or altered from any source
+ *     distribution.
  *
  * Created: Sun Jun  6 15:06:34 2004
  * </pre>
