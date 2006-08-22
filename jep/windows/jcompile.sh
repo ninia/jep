f4e6a340f2e1340bf53c86d855efd893b5e7a2e4
mrjohnson0 <mrjohnson0@86e5652c-ad1d-0410-bc08-f5e3af449c86>
2006-08-22 23:18:03 +0000

    simple script to compile java stuff

---
+++ jep/windows/jcompile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export JAVA_HOME='/cygdrive/c/Progra~1/Java/jdk1.5.0_08'
+export PATH=$JAVA_HOME/bin/:$PATH
+
+echo $PATH
+
+JAVAH=javah
+JAVAC=javac
+CLASSPATH=../
+JAVACOPT='-deprecation -classpath ../:./ext/bsf.jar'
+
+run() {
+    echo $*
+    $*
+
+    if [ $? != 0 ]; then
+        echo "Error"
+        exit 1
+    fi
+}
+
+pushd ../ >/dev/null
+
+if [ "$1" == "clean" ]; then
+    run rm -f *.class *.jar
+else
+#    for j in `ls *.java`; do
+#        run $JAVAC $JAVACOPT $j
+#    done
+    run $JAVAC $JAVACOPT *.java
+    run $JAVAH -o jep.h -classpath ../ jep.Jep
+    ./makejar.sh jep/ jep.jar
+fi
+
+popd >/dev/null
