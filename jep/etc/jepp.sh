d4856f85f9bcd0c1994babd01e4e3448e64ca3c2
Mike Johnson <mrjohnson@ubuntu.(none)>
2011-07-30 14:11:46 -0700

    version to 2.5

--- jep/etc/jepp.sh
+++ jep/etc/jepp.sh
@@ -1,3 +1,3 @@
 export LD_PRELOAD=/usr/`rpm --eval %_lib`/libpython2.7.so
-export CLASSPATH=$CLASSPATH:${exec_prefix}/lib/jepp/jepp-2.4.jar
+export CLASSPATH=$CLASSPATH:${exec_prefix}/lib/jepp/jepp-2.5.jar
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${exec_prefix}/lib/jepp
