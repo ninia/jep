5073698cf2d106eb61ac60b6709ae9dace41adec
Mike Johnson <mike@mrj0.com>
2013-05-10 12:55:42 -0700

    run tests directly from the script

--- runtests.py
+++ runtests.py
@@ -1,14 +1,13 @@
+#!/usr/bin/env jep
+
 import unittest
 
 import sys
-# embedding does not setup argv
-sys.argv = ['']
 # make sure we can run scripts from the current folder
 sys.path.insert(0, '')
 
 if __name__ == '__main__':
     try:
-        unittest.main(module='tests', argv=[''])
+        unittest.main(module='tests')
     except SystemExit:
         pass
-    
