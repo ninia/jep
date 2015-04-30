582dfdd32ce1a14cff9d95fef6e95cf81a3f9cc9
David Lovely <David.N.Lovely@Raytheon.com>
2015-04-30 13:24:32 -0500

    Only apply compiler patch in Windows

--- commands/build_ext.py
+++ commands/build_ext.py
@@ -8,14 +8,16 @@ from distutils.command.build_ext import build_ext as old_build_ext
 class build_ext (old_build_ext):
 
     def run(self):
-        import sys
-        import distutils
-        import distutils.ccompiler
-        from commands import msvc9compiler
-        
-        # See commands.msvc9compiler method comments for information on this override. 
-        distutils.ccompiler.compiler_class['msvc']= ('jepmsvccompiler', 'MSVCCompiler', "Microsoft Visual C++")
-        sys.modules['distutils.jepmsvccompiler'] = msvc9compiler
+        from commands.util import is_windows
+        if is_windows():
+            import sys
+            import distutils
+            import distutils.ccompiler
+            from commands import msvc9compiler
+            
+            # See commands.msvc9compiler method comments for information on this override. 
+            distutils.ccompiler.compiler_class['msvc']= ('jepmsvccompiler', 'MSVCCompiler', "Microsoft Visual C++")
+            sys.modules['distutils.jepmsvccompiler'] = msvc9compiler
         old_build_ext.run(self)
 
 # class build_ext
