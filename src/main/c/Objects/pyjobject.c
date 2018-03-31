eb017ec56ff76de53bf2e1c930a397461895c63a
Ben Steffensmeier <ben.steffensmeier@gmail.com>
2018-03-31 15:34:17 -0500

    Improve error message when setAttribute failes on pyjobjects.

--- src/main/c/Objects/pyjobject.c
+++ src/main/c/Objects/pyjobject.c
@@ -444,12 +444,19 @@ static int pyjobject_setattro(PyJObject *obj, PyObject *name, PyObject *v)
     }
 
     if (cur == NULL) {
-        PyErr_SetString(PyExc_RuntimeError, "No such field.");
+        PyErr_Format(PyExc_AttributeError, "'%s' object has no attribute '%s'.",
+                     PyString_AsString(obj->javaClassName), PyString_AsString(name));
         return -1;
     }
 
-    if (!PyJField_Check(cur)) {
-        PyErr_SetString(PyExc_TypeError, "Not a pyjfield object.");
+    if (!PyJField_Check(cur)) {    
+        if (PyJMethod_Check(cur) || PyJMultiMethod_Check(cur)) {
+            PyErr_Format(PyExc_AttributeError, "'%s' object cannot assign to method '%s'.",
+                         PyString_AsString(obj->javaClassName), PyString_AsString(name));
+        }else{
+            PyErr_Format(PyExc_AttributeError, "'%s' object cannot assign to attribute '%s'.",
+                         PyString_AsString(obj->javaClassName), PyString_AsString(name));
+        }
         return -1;
     }
 
