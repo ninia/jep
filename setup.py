ee36b519b4604fceae0dff13a2c5c2748daff72d
Ben Steffensmeier <ben.steffensmeier@gmail.com>
2018-05-07 17:30:57 -0500

    Minor cleanup

--- setup.py
+++ setup.py
@@ -98,6 +98,7 @@ if __name__ == '__main__':
                        'Programming Language :: Python :: 3.4',
                        'Programming Language :: Python :: 3.5',
                        'Programming Language :: Python :: 3.6', 
+                       'Programming Language :: Python :: 3.7', 
                        'Programming Language :: Python :: Implementation :: CPython',
                       ],
           ext_modules=[
