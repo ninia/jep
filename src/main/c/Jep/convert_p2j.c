48f128242b704b626e498c33fa8c1c87dab1e7e5
Jeremy Smith <jeremys@netflix.com>
2019-04-29 08:38:03 -0700

    Always return a `PyObject` if requested type is `PyObject`.
    Adds a higher-priority check if the requested type is PyObject. If it is, a PyObject will be returned.
    
    Fixes an issue with numpy arrays – currently, they cannot be retrieved as a `PyObject`, and attempting to do so results in a `TypeError` (since numpy conversion logic has no escape hatch if expected type is not a Java array nor Java `NDArray`)

--- src/main/c/Jep/convert_p2j.c
+++ src/main/c/Jep/convert_p2j.c
@@ -865,6 +865,16 @@ jobject PyObject_As_jobject(JNIEnv *env, PyObject *pyobject,
         if ((*env)->IsAssignableFrom(env, pyjobject->clazz, expectedType)) {
             return (*env)->NewLocalRef(env, pyjobject->object);
         }
+    } else if (PyCallable_Check(pyobject)) {
+        if (isFunctionalInterfaceType(env, expectedType)) {
+            return PyCallable_as_functional_interface(env, pyobject, expectedType);
+        } else if (PyErr_Occurred()) {
+            return NULL;
+        } else if ((*env)->IsAssignableFrom(env, JPYCALLABLE_TYPE, expectedType)) {
+            return PyCallable_As_JPyCallable(env, pyobject);
+        }
+    } else if ((*env)->IsAssignableFrom(env, expectedType, JPYOBJECT_TYPE)) {
+        return PyObject_As_JPyObject(env, pyobject);
 #if PY_MAJOR_VERSION < 3
     } else if (PyString_Check(pyobject)) {
         return pystring_as_jobject(env, pyobject, expectedType);
@@ -900,14 +910,6 @@ jobject PyObject_As_jobject(JNIEnv *env, PyObject *pyobject,
         return pyfastsequence_as_jobject(env, pyobject, expectedType);
     } else if (PyDict_Check(pyobject)) {
         return pydict_as_jobject(env, pyobject, expectedType);
-    } else if (PyCallable_Check(pyobject)) {
-        if (isFunctionalInterfaceType(env, expectedType)) {
-            return PyCallable_as_functional_interface(env, pyobject, expectedType);
-        } else if (PyErr_Occurred()) {
-            return NULL;
-        } else if ((*env)->IsAssignableFrom(env, JPYCALLABLE_TYPE, expectedType)) {
-            return PyCallable_As_JPyCallable(env, pyobject);
-        }
 #if JEP_NUMPY_ENABLED
     } else if (npy_array_check(pyobject)) {
         return convert_pyndarray_jobject(env, pyobject, expectedType);
