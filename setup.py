a389bce6085bf0e0520afec99fa2b46051d37c79
Ben Steffensmeier <ben.steffensmeier@gmail.com>
2021-02-03 20:12:01 -0600

    Remove support for Python 3.3 and 3.4

--- setup.py
+++ setup.py
@@ -92,8 +92,6 @@ if __name__ == '__main__':
                        'Topic :: Software Development',
                        'Programming Language :: Java',
                        'Programming Language :: Python',
-                       'Programming Language :: Python :: 3.3',
-                       'Programming Language :: Python :: 3.4',
                        'Programming Language :: Python :: 3.5',
                        'Programming Language :: Python :: 3.6', 
                        'Programming Language :: Python :: 3.7', 
