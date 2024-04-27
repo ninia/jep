0be8c74fec852c08e56961c7f71d07034a3697ab
Ben Steffensmeier <ben.steffensmeier@gmail.com>
2024-04-27 16:41:51 -0500

    Use build from newer setuptools when available.

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,5 +1,8 @@
-# https://github.com/pypa/setuptools/issues/2928
-from distutils.command.build import build
+try:
+    from setuptools.command.build import build
+except ImportError:
+    from distutils.command.build import build
+
 from commands.java import build_java
 
 class jep_build(build):
