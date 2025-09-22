6616ccdb7f50ff93d68bbd372c110b027eaf73e7
Ben Steffensmeier <ben.steffensmeier@gmail.com>
2025-09-21 21:32:41 -0500

    Change maven groupId to org.ninia.

--- commands/java.py
+++ commands/java.py
@@ -10,6 +10,7 @@ import fnmatch
 import traceback
 import sys
 import logging
+import textwrap
 from pathlib import Path
 
 from commands.util import configure_error
@@ -281,10 +282,29 @@ class build_jar(Command):
 
         self.setup_meta_inf(os.path.join('build', 'java'))
 
-        self.spawn([self.jar, '-cf', 'build/java/jep-{0}-sources.jar'.format(self.version), '-C', 'build/java/jep.src/main/java', 'jep', '-C', 'build/java', 'META-INF'])
-        self.spawn([self.jar, '-cfe', 'build/java/jep-{0}.jar'.format(self.version), 'jep.Run', '-C', 'build/java', 'jep', '-C', 'build/java', 'META-INF'])
-        self.spawn([self.jar, '-cf', 'build/java/jep-{0}-test-sources.jar'.format(self.version), '-C', 'build/java/jep.test.src/test/java', 'jep', '-C', 'build/java', 'META-INF'])
-        self.spawn([self.jar, '-cfe', 'build/java/jep-{0}-test.jar'.format(self.version), 'test.jep.Test', '-C', 'build/java/test', 'jep', '-C', 'build/java', 'META-INF'])
+        self.spawn([self.jar, '-c',
+                    f'--file=build/java/jep-{self.version}-sources.jar',
+                    '-C','build/java/jep.src/main/java', 'jep',
+                    '-C', 'build/java', 'META-INF'
+        ])
+        self.spawn([self.jar, '-c',
+                    f'--file=build/java/jep-{self.version}.jar',
+                    '--main-class=jep.Run',
+                    '--manifest=build/java/MANIFEST-ADD.MF',
+                    '-C', 'build/java', 'jep',
+                    '-C', 'build/java', 'META-INF'
+        ])
+        self.spawn([self.jar, '-c', 
+                    f'--file=build/java/jep-{self.version}-test-sources.jar',
+                    '-C', 'build/java/jep.test.src/test/java', 'jep',
+                    '-C', 'build/java', 'META-INF'
+        ])
+        self.spawn([self.jar, '-c',
+                    f'--file=build/java/jep-{self.version}-test.jar',
+                    '--main-class=test.jep.Test',
+                    '-C', 'build/java/test', 'jep',
+                    '-C', 'build/java', 'META-INF'
+        ])
 
 
     def run(self):
@@ -294,13 +314,15 @@ class build_jar(Command):
             logging.debug('skipping packing jar files (up to date)')
 
     def setup_meta_inf(self, dest_dir):
-        pom_dir = os.path.join(dest_dir, 'META-INF', 'maven', 'black.ninia', 'jep')
+        pom_dir = os.path.join(dest_dir, 'META-INF', 'maven', 'org.ninia', 'jep')
         if not os.path.exists(pom_dir):
             os.makedirs(pom_dir)
         shutil.copy('pom.xml', pom_dir)
-        pom_props ="""artifactId=jep
-groupId=black.ninia
-version="""
-        pom_props += self.version
+        pom_props = textwrap.dedent(f"""\
+            artifactId=jep
+            groupId=org.ninia
+            version={self.version}""")
         with open(os.path.join(pom_dir, 'pom.properties'), 'w') as f:
             f.write(pom_props)
+        with open(os.path.join(dest_dir, 'MANIFEST-ADD.MF'), 'w') as f:
+            f.write("Automatic-Module-Name: org.ninia.jep\n")
